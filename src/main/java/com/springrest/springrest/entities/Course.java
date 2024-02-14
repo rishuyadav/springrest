aa90fec2c338cb77c2ee667caf8a216869715b43
Rishu Yadav <rishuss1212@gmail.com>
2024-02-14 16:31:32 +0530

    fix: add lombok removing redundant code

--- src/main/java/com/springrest/springrest/entities/Course.java
+++ src/main/java/com/springrest/springrest/entities/Course.java
@@ -3,8 +3,11 @@ package com.springrest.springrest.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jdk.jfr.DataAmount;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
+@Entity @Getter @Setter @NoArgsConstructor
 public class Course {
     @Id
     private long id;
@@ -16,31 +19,5 @@ public class Course {
         this.title = title;
         this.description = description;
     }
-    public Course(){
-        super();
-    }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
